75226ed6d7c06ae6c19e39e70c041708a11d8244
Jinhao Ping <pingjinhao@gmail.com>
2016-09-17 19:18:38 -0500

    more css on landing page

---
+++ hw3/dist/main.js
@@ -0,0 +1,87 @@
+var array = ["http://www.pngall.com/wp-content/uploads/2016/06/Pokemon-PNG.png", 
+			"http://www.freeiconspng.com/uploads/pokemon-png-13.png", 
+			"http://www.freeiconspng.com/uploads/pokemon-png-28.png",
+ 			"http://vignette1.wikia.nocookie.net/pokemon/images/d/d7/Ash_and_His_Friend.png/revision/latest?cb=20110118225523", 
+ 			"http://pokemon.supercheats.com/artwork/383.png"];
+var start1;
+var start2;
+var start3;
+var start4;
+var start5;
+var time1;
+var time2;
+var time3;
+var time4;
+var time5;
+var times = [time1, time2, time3, time4, time5] ;
+var starts = [start1, start2, start3, start4, start5];
+var images = ["image1","image2","image3","image4","image5"]
+var btns = ["btn1","btn2","btn3","btn4","btn5"]
+var mapBtn2Start = {};
+var mapBtn2Time = {};
+var mapBtn2Image = {};
+
+/*
+when function onloads, start to setIntervals initially.
+*/
+window.onload = function() {
+	init()
+	map()
+}
+
+
+function init() {
+	var j = 0;
+	while (j < 5) {
+		times[j] = Math.floor((Math.random() * 5) + 1) * 1000;
+		starts[j] = setInterval(changeImage,times[j],images[j],"");
+		j++;
+	}
+}
+
+/*
+three hash tables used to match image id and their time and start variable
+*/
+function map() {
+	var i = 0;
+	while (i < 5) {
+		mapBtn2Time[btns[i]] = times[i]
+		mapBtn2Start[btns[i]] = starts[i]
+		mapBtn2Image[btns[i]] = images[i]
+		i++
+	}
+}
+
+/*
+check which state it is for this particular click
+*/
+function checkValid(id) {
+		if (document.getElementById(id).innerHTML == "Stop" ) {
+			Stop(mapBtn2Start[id]);
+			document.getElementById(id).innerHTML = "Start";
+
+		} else if (document.getElementById(id).innerHTML == "Start") {
+			mapBtn2Time[id] = Math.floor((Math.random() * 5) + 1) * 1000;
+			mapBtn2Start[id] = setInterval(changeImage, mapBtn2Time[id], mapBtn2Image[id], "");
+			document.getElementById(id).innerHTML = "Stop";
+		}
+	
+}
+
+function Stop(x) {
+	clearInterval(x);
+}
+
+/*
+rotate images in the same order. Always show the image next to the current image
+*/
+function changeImage(param1, param2){
+	var i = 0
+	while (i < 5) {
+		if (document.getElementById(param1).src == array[i]) {
+			document.getElementById(param1).src = array[(i+1)%5];
+			break;
+		}
+		i++;
+	}
+}
---
+++ hw3/dist/profile.js
@@ -0,0 +1,126 @@
+
+/* 
+  First check validation
+  Then check which fields to be updated
+*/
+function update() {
+	var valid = checkValid();
+	if (valid){
+		updateName()
+		updateEmail()
+		updatePhone()
+		updateZipcode()
+		updatePassword()
+	}
+}
+
+/*
+check email input patten
+check phone number input pattern
+check zipcode pattern
+check whether password are the same or not
+*/
+function checkValid() {
+	if(!document.getElementById("email").value.match(/\S+@\S+\.\S+/)){
+		if (document.getElementById("email").value != "") {
+			alert("Email Format is wrong!")
+			return false;
+		} 
+	}
+	if (!document.getElementById("phone_number").value.match("^\\d{3}-\\d{3}-\\d{4}$")) {
+		if (document.getElementById("phone_number").value != "") {
+			alert("Phone Format is wrong!")
+			return false;
+		}
+	}
+	if (!document.getElementById("zipcode").value.match("^\\d{5}$")) {
+		if (document.getElementById("zipcode").value != "") {
+			alert("Zipcode Format is wrong!")
+			return false;
+		}
+	}
+	var password = document.getElementById("password")
+	var password2 = document.getElementById("password2")
+	if (password.value == "" && password2.value != "") {
+		alert("Password is empty !")
+		return false;
+	} else if (password2.value == "" && password.value != ""){
+		alert("Confirmation Password is empty !")
+		return false;
+	}
+	return true;
+}
+
+function updateName() {
+	var old_name = document.getElementById("s_display_name")
+	var edit_name = document.getElementById("display_name")
+	if (edit_name.value != "") {
+		if (edit_name.value === old_name.innerHTML) {
+			alert("Display Name is the same as old one. No change is made !");
+			edit_name.value = ""
+		} else {
+			alert("Display Name is going to be changed from " + old_name.innerHTML + " to " + edit_name.value )
+			old_name.innerHTML = edit_name.value;
+			edit_name.value = ""
+		}
+	}
+}
+
+function updateEmail() {
+	var old_email = document.getElementById("s_email")
+	var edit_email = document.getElementById("email")
+	if (edit_email.value != "") {
+		if (edit_email.value === old_email.innerHTML) {
+			alert("New Email entered is the same as old one. No changes is made !")
+			edit_email.value = ""
+		} else {
+			alert("Email is going to be changed from " + old_email.innerHTML + " to " + edit_email.value)
+			old_email.innerHTML = edit_email.value
+			edit_email.value = ""
+		}
+	}
+}
+
+function updatePhone() {
+	var old_phone = document.getElementById("s_phone_number")
+	var edit_phone = document.getElementById("phone_number")
+	if (edit_phone.value != "") {
+		if (edit_phone.value === old_phone.innerHTML) {
+			alert("Phone number is unchanged!")
+			edit_phone.value = ""
+		} else {
+			alert("Phone number is going to be changed from " + old_phone.innerHTML + " to " + edit_phone.value)
+			old_phone.innerHTML = edit_phone.value
+			edit_phone.value = ""
+		}
+	}
+}
+
+function updateZipcode() {
+	var old_zip = document.getElementById("s_zipcode")
+	var edit_zip = document.getElementById("zipcode")
+	if (edit_zip.value != "") {
+		if (edit_zip.value === old_zip.innerHTML) {
+			alert("Zipcode is unchanged !")
+			edit_zip.value = ""
+		} else {
+			alert("Zipcode is going to be changed from " + old_zip.innerHTML + " to " + edit_zip.value)
+			old_zip.innerHTML = edit_zip.value
+			edit_zip.value = ""
+		}
+	}
+}
+
+function updatePassword() {
+	var password = document.getElementById("password")
+	var password2 = document.getElementById("password2")
+	if (password.value != "" || password2.value != "") {
+		if (password.value == password2.value) {
+			alert("Password updated successfully!")
+			password.value = ""
+			password2.value = ""
+		} else {
+			alert("Passwords are not the same")
+		}
+	}
+}
