6250a560341018961cb164762d8b622a637af82c
Jinhao Ping <pingjinhao@gmail.com>
2016-10-23 23:35:39 -0500

    main done

--- hw5/src/components/main/followingActions.js
+++ hw5/src/components/main/followingActions.js
@@ -1,20 +1,20 @@
 import Promise from 'bluebird'
 import Action, { resource, updateError } from '../../actions'
 
-export function delFollower(name) { return fetchFollowers('DELETE', name) }
-export function addFollower(name) { return fetchFollowers('PUT', name) }
 
 export function fetchFollowers(method, name) {
     return (dispatch, getState) => {
         if (method == 'PUT' && getState().followers.followers[name]) {
-            return dispatch(updateError(`Already following ${name}`))
+            return dispatch(updateError(`You are already following ${name}`))
         }
 
         resource(method ? method : 'GET', 'following' + (name ? '/' + name : ''))
+        
+
         .then((response) => {
 
             if (method == 'PUT' && response.following.indexOf(name) < 0) {
-                return dispatch(updateError(`${name} is not a valid user`))
+                return dispatch(updateError(`${name} is not a real user`))
             }
 
             const followers = response.following.reduce((o, v, i) => { o[v] = {name: v}; return o }, {})
@@ -22,20 +22,20 @@ export function fetchFollowers(method, name) {
 
             const headlinePromise = resource('GET', `headlines/${followerList}`)
                 .then((response) => {
-                    response.headlines.forEach((s) => {
-                        const user = followers[s.username]
+                    response.headlines.forEach((u) => {
+                        const user = followers[u.username]
                         if (user) {
-                            user.headline = s.headline
+                            user.headline = u.headline
                         }
                     })
                 })
 
             const avatarPromise = resource('GET', `avatars/${followerList}`)
                 .then((response) => {
-                    response.avatars.forEach((s) => {
-                        const user = followers[s.username]
+                    response.avatars.forEach((u) => {
+                        const user = followers[u.username]
                         if (user) {
-                            user.avatar = s.avatar
+                            user.avatar = u.avatar
                         }
                     })
                 })
@@ -47,4 +47,14 @@ export function fetchFollowers(method, name) {
             dispatch(updateError(`Error appears while getting your list of followed users ${err}`))
         })
     }
+}
+
+
+
+export function delFollower(name) { 
+    return fetchFollowers('DELETE', name) 
+}
+
+export function addFollower(name) { 
+    return fetchFollowers('PUT', name) 
 }
