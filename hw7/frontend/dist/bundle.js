d19b94026cac44c09f1b4c46ed3a4a7466900e9d
Jinhao Ping <pingjinhao@gmail.com>
2016-11-22 21:58:34 -0600

    test

--- hw7/frontend/dist/bundle.js
+++ hw7/frontend/dist/bundle.js
@@ -30,6 +30,7 @@ function r(e,t){if(!a.canUseDOM||t&&!("addEventListener"in document))return!1;va
 	 * The above copyright notice and this permission notice shall be included in
 	 * all copies or substantial portions of the Software.
 	 * 
+	 *
 	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
